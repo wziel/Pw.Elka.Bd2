085acf00a84d64a4cbad2b71771b393af791fa0f
Wojciech Zieliński <zielinskiwt@outlook.com>
2015-12-20 16:44:31 +0100

    Create C# project for tests.

---
+++ Pw.Elka.Bd2/Pw.Elka.Bd2.Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pw.Elka.Bd2.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
