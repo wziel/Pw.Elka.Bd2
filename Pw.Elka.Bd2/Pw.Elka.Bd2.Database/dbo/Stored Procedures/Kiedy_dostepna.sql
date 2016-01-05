a214285f5865bcffd4d6ecded8d7a8406e4a1265
Wojciech Zieliński <zielinskiwt@outlook.com>
2016-01-05 18:50:58 +0000

    Fix procedure Kiedy_dostepna

--- Pw.Elka.Bd2/Pw.Elka.Bd2.Database/dbo/Stored Procedures/Kiedy_dostepna.sql
+++ Pw.Elka.Bd2/Pw.Elka.Bd2.Database/dbo/Stored Procedures/Kiedy_dostepna.sql
@@ -5,7 +5,7 @@
 -- =============================================
 CREATE PROCEDURE [dbo].[Kiedy_dostepna] 
 	-- Add the parameters for the stored procedure here
-	@id_pozycja smallint
+	@id_pozycja int
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -13,5 +13,5 @@ BEGIN
 	SET NOCOUNT ON;
 
     -- Insert statements for procedure here
-	SELECT Pozycja.dostepna_od from Pozycja where Pozycja.id_pozycja = @id_pozycja 
+	SELECT Pozycja.nazwa, Pozycja.dostepna_od from Pozycja where Pozycja.id_pozycja = @id_pozycja 
 END
